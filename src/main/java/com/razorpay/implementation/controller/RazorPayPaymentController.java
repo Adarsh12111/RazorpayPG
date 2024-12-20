69ec39b752a7e4c1ee534dcf11e693921cc7295a
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-12-20 13:59:20 +0530

    added web hook

--- src/main/java/com/razorpay/implementation/controller/RazorPayPaymentController.java
+++ src/main/java/com/razorpay/implementation/controller/RazorPayPaymentController.java
@@ -4,6 +4,9 @@ import com.razorpay.Order;
 import com.razorpay.RazorpayException;
 import com.razorpay.implementation.dto.InitiatedOrderDTO;
 import com.razorpay.implementation.service.PaymentGatewayImplementationService;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +15,9 @@ import java.util.Map;
 @RestController
 @RequestMapping(path = "/payment")
 public class RazorPayPaymentController {
+
+    private static  final Logger log = LoggerFactory.getLogger(RazorPayPaymentController.class);
+
 
     @Autowired
     private PaymentGatewayImplementationService paymentGatewayImplementationService;
@@ -25,5 +31,27 @@ public class RazorPayPaymentController {
         initiatedOrderDTO.setFirst(name);
         initiatedOrderDTO.setLastName("Tripathi");
         return paymentGatewayImplementationService.createOrder(initiatedOrderDTO);
+    }
+
+
+    @PostMapping("/callback")
+    public String handleCallBack(@RequestBody(required = false) String payload){
+        try {
+            // Parse the JSON payload
+            JSONObject json = new JSONObject(payload);
+            String event = json.getString("event");
+
+            log.info("payment payload {}",payload);
+
+            if ("payment.captured".equals(event)) {
+                String paymentId = json.getJSONObject("payload").getJSONObject("payment").getString("id");
+                // Process the payment ID (update database, send email, etc.)
+                System.out.println("Payment captured: " + paymentId);
+            }
+            return "Callback processed successfully";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Callback failed";
+        }
     }
 }
